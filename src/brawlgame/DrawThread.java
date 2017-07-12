6d165f82e95ebb33d73982a587cff6d1992017e9
bakajaan <bakajaan@yahoo.co.jp>
2017-07-13 07:45:08 +0900

    [fix]キャラクターの挙動を修正
    キャラクターがマップ外に出ないよう変更した。

--- src/brawlgame/DrawThread.java
+++ src/brawlgame/DrawThread.java
@@ -30,90 +30,88 @@ public class DrawThread extends Thread {
             long oldTime = System.currentTimeMillis();//描画前時間の取得
             //パネルの移動
             if (GP.isDrawEnable()) {
+                int move = 0;
                 if (GP.getTurnMode() == GP.getMode()) {
                     if (GP.getAX() > GP.getBX()) {
                         if (GP.getAX() - GP.getBX() < 980) {
-                            int move = GP.getGameP().getX() + (GP.getBX() + (GP.getAX() - GP.getBX()) / 2) - 490 + GP.getCharSize() / 2;
+                            move = GP.getGameP().getX() + (GP.getBX() + (GP.getAX() - GP.getBX()) / 2) - 490 + GP.getCharSize() / 2;
                             if (move > 32) {
                                 move = 32;
                             }
                             if (move < -32) {
                                 move = -32;
                             }
-                            GP.getGameP().setLocation(GP.getGameP().getX() - move, 0);
                         } else {
-                            int move = GP.getGameP().getX() + GP.getAX() - 200 + GP.getCharSize() / 2;
+                            move = GP.getGameP().getX() + GP.getAX() - 200 + GP.getCharSize() / 2;
                             if (move > 32) {
                                 move = 32;
                             }
                             if (move < -32) {
                                 move = -32;
                             }
-                            GP.getGameP().setLocation(GP.getGameP().getX() - move, 0);
                         }
                     } else {
                         if (GP.getBX() - GP.getAX() < 980) {
-                            int move = GP.getGameP().getX() + (GP.getAX() + (GP.getBX() - GP.getAX()) / 2) - 490 + GP.getCharSize() / 2;
+                            move = GP.getGameP().getX() + (GP.getAX() + (GP.getBX() - GP.getAX()) / 2) - 490 + GP.getCharSize() / 2;
                             if (move > 32) {
                                 move = 32;
                             }
                             if (move < -32) {
                                 move = -32;
                             }
-                            GP.getGameP().setLocation(GP.getGameP().getX() - move, 0);
                         } else {
-                            int move = GP.getGameP().getX() + GP.getAX() - 200 + GP.getCharSize() / 2;
+                            move = GP.getGameP().getX() + GP.getAX() - 200 + GP.getCharSize() / 2;
                             if (move > 32) {
                                 move = 32;
                             }
                             if (move < -32) {
                                 move = -32;
                             }
-                            GP.getGameP().setLocation(GP.getGameP().getX() - move, 0);
                         }
                     }
                 } else {
                     if (GP.getAX() > GP.getBX()) {
                         if (GP.getAX() - GP.getBX() < 980) {
-                            int move = GP.getGameP().getX() + (GP.getBX() + (GP.getAX() - GP.getBX()) / 2) - 490 + GP.getCharSize() / 2;
+                            move = GP.getGameP().getX() + (GP.getBX() + (GP.getAX() - GP.getBX()) / 2) - 490 + GP.getCharSize() / 2;
                             if (move > 32) {
                                 move = 32;
                             }
                             if (move < -32) {
                                 move = -32;
                             }
-                            GP.getGameP().setLocation(GP.getGameP().getX() - move, 0);
                         } else {
-                            int move = GP.getGameP().getX() + GP.getBX() - 200 + GP.getCharSize() / 2;
+                            move = GP.getGameP().getX() + GP.getBX() - 200 + GP.getCharSize() / 2;
                             if (move > 32) {
                                 move = 32;
                             }
                             if (move < -32) {
                                 move = -32;
                             }
-                            GP.getGameP().setLocation(GP.getGameP().getX() - move, 0);
                         }
                     } else {
                         if (GP.getBX() - GP.getAX() < 980) {
-                            int move = GP.getGameP().getX() + (GP.getAX() + (GP.getBX() - GP.getAX()) / 2) - 490 + GP.getCharSize() / 2;
+                            move = GP.getGameP().getX() + (GP.getAX() + (GP.getBX() - GP.getAX()) / 2) - 490 + GP.getCharSize() / 2;
                             if (move > 32) {
                                 move = 32;
                             }
                             if (move < -32) {
                                 move = -32;
                             }
-                            GP.getGameP().setLocation(GP.getGameP().getX() - move, 0);
                         } else {
-                            int move = GP.getGameP().getX() + GP.getBX() - 200 + GP.getCharSize() / 2;
+                            move = GP.getGameP().getX() + GP.getBX() - 200 + GP.getCharSize() / 2;
                             if (move > 32) {
                                 move = 32;
                             }
                             if (move < -32) {
                                 move = -32;
                             }
-                            GP.getGameP().setLocation(GP.getGameP().getX() - move, 0);
                         }
                     }
+                }
+                if (GP.getGameP().getX() - move > 0) {
+                    GP.getGameP().setLocation(0, 0);
+                } else {
+                    GP.getGameP().setLocation(GP.getGameP().getX() - move, 0);
                 }
                 GP.getGameP().repaint();
                 GP.setDrawEnable(false);
